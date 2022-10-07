ba8ad0e01e875bf23e95d5bb155ac7257c17d71e
Giorgos Tziouliou <72397660+GeorghiosTziouliou@users.noreply.github.com>
2022-10-07 18:34:49 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,127 +2,144 @@ import socket
 import threading
 
 Port = 6667
-#Server ="0000:0000:0000:0000:0000:0000:0000:0001"
-#ADDRESS = (Server, Port)
+# Server ="0000:0000:0000:0000:0000:0000:0000:0001"
+# ADDRESS = (Server, Port)
 ADDRESS = ('localhost', Port)
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 print('Server Starting on {} port {}'.format(*ADDRESS))
 sock.bind(ADDRESS)
 
-#channel
+
+# channel
 class Channel:
 
-    #initialise object attributes
+    # initialise object attributes
     def __init__(self, name):
         self.name = name
         self.users = []
 
-    #add client to list
+    # add client to list
     def join(self, user):
         self.users.append(user)
         for u in self.users:
             sendMSG(f'user {user.nickname} has joined the chat', u)
 
-    #remove client
+    # remove client
     def leave(self, user):
         self.users.remove(user)
         for u in self.users:
             sendMSG(f'user {user.nickname} has left the channel', u)
 
-    #private message
+    # private message
     def privateMessageChannel(self, user, data):
         if data == "":
             return
         m = f"|private| {user.nickname}: {data}"
 
-        # create channel
-        class User:
-            def __init__(self, socket, addr, username, nickname, realname):
-                self.socket = socket
-                self.address = addr
-                self.username = username
-                self.nickname = nickname
-                self.realname = realname
-
-        channels = [Channel("Test"), Channel("Unicorn")]
-
-        def connectUser(s):
-            sock.listen()
-            while True:
-
-                try:
-                    socket, addr = s.accept()
-                except:
-                    print(f'{str(addr)} error connecting')
-
-                print(f"{str(addr)} has connected")
-
-                username = ""
-                nickname = ""
-                realname = ""
-
-                while True:
-
-                    msg = socket.recv(1024).decode('utf-8')
-
-                    if "USER" not in msg:
-                        socket.send("please enter a correct USER command".encode('utf-8'))
-                        continue
-
-                    for c in channels:
-                        for u in c.users:
-                            if u.username == username:
-                                socket.send(f'sorry username already taken, please try again'.encode('utf-8'))
-                                continue
-
-                    cmd = msg.split()
-                    username = cmd[1]
-                    realname = cmd[4]
-
-                    socket.send("Username Valid".encode('utf-8'))
-
-                    break
 
 # Listen for messages
 print('waiting for a Client Connection')
 
-while True:
 
-    try:
-        connection, client_address = sock.accept()
 
-    except:
-        print('Error')
 
-        # Receive Messages
+# create channel
+class User:
+    def __init__(self, socket, addr, username, nickname, realname):
+        self.socket = socket
+        self.address = addr
+        self.username = username
+        self.nickname = nickname
+        self.realname = realname
+
+#predefined channels
+channels = [Channel("test"), Channel("unicorn")]
+
+
+def connectUser(s):
+    s.listen()
+    while True:
+
+        try:
+            socket, addr = s.accept()
+        except:
+            print(f'{str(addr)} error connecting')
+
+        print(f"{str(addr)} has connected")
+
+        username = ""
+        nickname = ""
+        realname = ""
+
         while True:
-            data = connection.recv(16)
-            print('received {!r}'.format(data))
 
-            if data:
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
+            msg = socket.recv(1024).decode('utf-8')
+            print(msg)
 
-            username = ""
-            nickname = ""
-            realname = ""
+            if "USER" not in msg:
+                socket.send("please set your nickname using /nick <your nickname>\n".encode('utf-8'))
+                continue
 
-#send messages
+            for c in channels:
+                for u in c.users:
+                    if u.username == username:
+                        socket.send(f'sorry username {user} already taken, please try again'.encode('utf-8'))
+                        continue
+
+            cmd = msg.split()
+
+            username = cmd[1]
+            realname = cmd[4]
+
+            socket.send("Username Valid".encode('utf-8'))
+
+            break
+
+        while True:
+            msg = socket.recv(1024).decode('utf-8')
+            print(msg)
+
+            if "NICK" not in msg:
+                socket.send("please enter a correct NICK command".encode('utf-8'))
+                continue
+
+            cmd = msg.split()
+            nickname = cmd[1]
+
+            break
+
+        user = User(socket, addr, username, nickname, realname)
+
+        thread = handleClient(user)
+        thread.start()
+
+
+# send messages
 def sendMSG(msg, user):
     try:
         user.socket.send(msg.encode('utf-8'))
     except:
         user.socket.close()
-        print(f'user {user.nickname} disconnected with error')
+        print(f'user {user.nickname} disconnected unexpectedly')
         for c in channels:
             for u in c.users:
                 if u == user:
                     c.leave(user)
         exit()
 
-#join a channel
+
+# Client Commands
+def listUserCommands():
+    msg = "\nCOMMANDS"
+    msg = msg + "\nJOIN - Join a Channel. You can only join #test and #unicorn"
+    msg = msg + "\nPING - PING PONG"
+    msg = msg + "\nPMMSG - private message to a Client"
+    msg = msg + "\nEXIT - you exit\n"
+    return msg
+
+
+# join a channel
 def join(user, msg):
     chanName = msg.strip().split()[1][1:]
 
@@ -135,7 +152,7 @@ def join(user, msg):
             channel = c
 
     if channel == None:
-        data("\nPlease enter a valid channel name", user)
+        sendMSG("\nPlease enter a valid channel name", user)
         return
 
     for c in channels:
@@ -147,7 +164,7 @@ def join(user, msg):
     print(f'user {user.nickname} has has joined channel {channel.name}')
 
 
-#private messages
+# private messages
 def pm(user, msg):
     cmd = msg.split(":")
     message = cmd[1]
@@ -162,18 +179,23 @@ def pm(user, msg):
             for channel in channels:
                 for u in channel.users:
                     if u.nickname == recipient:
-                        data(f"|Private| {user.nickname}: {message}", u)
+                        sendMSG(f"|Private| {user.nickname}: {message}", u)
 
-#Client Commands
-def listUserCommands():
-    msg = "COMMANDS"
-    msg = msg + "\nJOIN - Join a Channel"
-    msg = msg + "\nPING - PING PONG"
-    msg = msg + "\nPMMSG - private message to a Client"
-    msg = msg + "\nEXIT - you exit\n"
-    return msg
 
-#Command Handling
+def safePipe(user):
+    try:
+        return user.socket.recv(1024).decode('utf-8')
+    except:
+        user.socket.close()
+        print(f'user {user.nickname} disconnected unexpectedly')
+        for c in channels:
+            for u in c.users:
+                if u == user:
+                    c.leave(user)
+        exit()
+
+
+# Command Handling
 class handleClient(threading.Thread):
     def __init__(self, user):
         threading.Thread.__init__(self)
@@ -185,24 +207,28 @@ class handleClient(threading.Thread):
 
         self.channel = None
 
-        data(listUserCommands(), self.user)
+        sendMSG(listUserCommands(), self.user)
 
-while True:
+        while True:
+            msg = safePipe(self.user)
 
-    if "JOIN" in msg:
-        join(self.user, msg)
-    elif msg == "PING":
-        sendMSG("PONG", user)
-    elif msg == "LIST":
-        for u in self.channel.users:
-            sendMSG(f'{u.nickname}\n', self.user)
-    elif "PMMSG" in msg:
-        pm(self.user, msg)
-    elif msg == "EXIT":
-        self.channel.leave(self.user)
-        sendMSG("EXIT", self.user)
-        self.user.socket.shutdown
-        print(f'user {self.user.nickname} has disconnected')
-        break
-    else:
-        self.channel.messageChannel(self.user, msg)
+            if "JOIN" in msg:
+                join(self.user, msg)
+            elif msg == "PING":
+                sendMSG("PONG", user)
+            elif msg == "LIST":
+                for u in self.channel.users:
+                    sendMSG(f'{u.nickname}\n', self.user)
+            elif "PMMSG" in msg:
+                pm(self.user, msg)
+            elif msg == "EXIT":
+                self.channel.leave(self.user)
+                sendMSG("EXIT", self.user)
+                self.user.socket.shutdown
+                print(f'user {self.user.nickname} has disconnected')
+                break
+            else:
+                print(msg)
+
+
+connectUser(sock)
