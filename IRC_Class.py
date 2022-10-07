34566bbd728a8a12fe0c13dd99321b153adcbd18
GeorghiosTziouliou <2412649@dundee.ac.uk>
2022-10-07 07:51:15 +0100

    Reformated the miniircd file for easier creation of the final one

--- IRC_Class.py
+++ IRC_Class.py
@@ -2,40 +2,39 @@ import socket
 import sys
 import time
 
-#Group 9
-#A class containing important functions for the IRC bot to work properly
+
+# Group 9
+# A class containing important functions for the IRC bot to work properly
 
 
 class IRC_Functs:
-    soc = socket.socket()#create the socket
+    soc = socket.socket()  # create the socket
 
     def __init__(self):
-        #initialise the socket
+        # initialise the socket
         self.soc = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-
-    #function for connecting to the IRC server
+    # function for connecting to the IRC server
     def connect(self, serverAddress, portNum, chanName, nickname):
-        #server connection
+        # server connection
         self.soc.connect((serverAddress, portNum))
 
-
-        #user setup
-        self.soc.send(bytes("USER " + nickname + " " + nickname +" " + nickname + " :python\n", "UTF-8"))#fill in the nickname form
+        # user setup
+        self.soc.send(bytes("USER " + nickname + " " + nickname + " " + nickname + " :python\n",
+                            "UTF-8"))  # fill in the nickname form
         self.soc.send(bytes("NICK " + nickname + "\n", "UTF-8"))
-        
-        
-        #join a channel
+
+        # join a channel
         self.soc.send(bytes("JOIN " + chanName + "\n", "UTF-8"))
 
-    #ping pong with server
+    # ping pong with server
     def ping(self):
         time.sleep(1)
 
-        #get response from server
+        # get response from server
         servResp = self.soc.recv(2040).decode("UTF-8")
 
         if servResp.find('PING') != -1:
-            self.soc.send(bytes('PONG ' + servResp.split().decode("UTF-8")  [1] + '\r\n', "UTF-8"))
-         
+            self.soc.send(bytes('PONG ' + servResp.split()[1] + '\r\n', "UTF-8"))
+
         return servResp
